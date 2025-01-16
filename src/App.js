56950d7b9480ceb2affd60871cf7411490724576
maggie januarie <livjanua@gmail.com>
2025-01-16 16:23:38 +0200

    changed paragraph to footer and added links.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 
-
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
 
-      <p>This project was coded by Olivia J and is open-sourced on Github.</p>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/livjanua">Olivia J</a> and is open-sourced
+        on{" "}
+        <a href="https://github.com/livjanua/react-weather-app" target="_blank">
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
-
-
