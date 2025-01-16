2dc388897b84a81df9867a201eb12c59706dbe36
maggie januarie <livjanua@gmail.com>
2025-01-16 17:08:57 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
+    <div className="container">
+      <div className="App">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/livjanua">Olivia J</a> and is open-sourced
-        on{" "}
-        <a href="https://github.com/livjanua/react-weather-app" target="_blank">
-          Github
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/livjanua">Olivia J</a> and is open-sourced
+          on{" "}
+          <a
+            href="https://github.com/livjanua/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
