878c7eb0c1219b47ac5a1f74820da77073e58200
maggie januarie <livjanua@gmail.com>
2025-01-16 15:23:20 +0200

    Removed unnecessary files, added H1 and paragraph.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <p>This project was coded by Olivia J and is open-sourced on Github.</p>
+    </div>
+  );
+}
+
+
