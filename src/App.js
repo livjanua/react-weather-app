6007bb62407afae30f51eb1dc263416bbb852b01
maggie januarie <livjanua@gmail.com>
2025-01-18 12:31:36 +0200

    Built the HTML structure.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="container">
       <div className="App">
-        <h1>Weather App</h1>
-
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/livjanua">Olivia J</a> and is open-sourced
