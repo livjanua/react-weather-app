04ae0f2d6bb14010dc4ab969a559a3197026c2e4
maggie januarie <livjanua@gmail.com>
2025-01-20 12:17:05 +0200

    Edited Footer and added a link.

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ export default function App() {
         <Weather defaultCity="Kimberley" />
         <footer>
           This project was coded by{" "}
-          <a href="https://github.com/livjanua">Olivia J</a> and is open-sourced
-          on{" "}
+          <a
+            href="https://github.com/livjanua"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Olivia J
+          </a>{" "}
+          and is open-sourced on{" "}
           <a
             href="https://github.com/livjanua/react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
             Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://shiny-tarsier-6ef2be.netlify.app/"
+            target="_blank"
+            ref="noreferrer"
+          >
+            Netlify
           </a>
         </footer>
       </div>
